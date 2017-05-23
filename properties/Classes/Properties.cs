e6c0197bb61e14618a2fb0da5c9a21dd17d8ad5e
Michal Artur Piotr Pomarkiewicz <michalpomar@gmail.com>
2017-05-23 12:51:57 +0100

    changed one name

--- properties/Classes/Properties.cs
+++ properties/Classes/Properties.cs
@@ -3,49 +3,14 @@ using AbstractProperty;
 
 
 
-namespace properties
+namespace PropertyList
 {
     class Properties
     {
 
     }
 
-    //    static void AddProperty() //Adds an information about new property with all the necessary data into the database.
-    //    {
-    //        String type;
-    //        Console.WriteLine("What type of property is this?");
-
-    //        type = Console.ReadLine();
-
-    //        if (type == "Apartment" || type == "apartment")
-    //        {
-
-    //            Console.WriteLine();
-    //        }
-    //        else if (type == "Farm" || type == "farm")
-    //        {
-
-    //        }
-    //        else if (type == "Flat" || type == "flat")
-    //        {
-
-    //        }
-    //        else if (type == "Hotel" || type == "hotel")
-    //        {
-
-    //        }
-    //        else if (type == "House" || type == "house")
-    //        {
-
-    //        }
-    //        else if (type == "Restaurant" || type == "restaurant")
-    //        {
-
-    //        }
-
-
-
-    //    }
+    
 
 
 }
